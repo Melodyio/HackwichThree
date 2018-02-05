61625809ea51905f222b21701ae99f8af7df3ceb
CM Student <cmstudent@UHWO-ACM-19.local>
2018-02-05 10:52:58 -1000

    Added about icon to item 1

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//set firstLabel to the string "About"
+        
+self.firstLabel.text = "About"
+        
     }
 
     override func didReceiveMemoryWarning() {
